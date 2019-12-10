d9d66d4f8ca73bef4846823d27e5226671fbf3f4
Nick Ramsay <NickRamsay1989@gmail.com>
2019-12-10 22:36:39 +1100

    Adding initial layouts and Bootstrap code

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Google Books</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link" href="#">Search</a>
+            <a class="nav-item nav-link" href="#">Saved</a>
+          </div>
+        </div>
+      </nav>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Google Books Search</h1>
+          <p class="lead">Search for books that are available on <a href="https://books.google.com/">Google Books</a></p>
+        </div>
+      </div>
     </div>
   );
 }
