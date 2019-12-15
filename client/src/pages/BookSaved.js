f43a8d1925df79c8517d4a2d03c6931c807ea1c9
Nick Ramsay <NickRamsay1989@gmail.com>
2019-12-15 16:00:15 +1100

    Added conditional rendering for 'No saved books'

--- client/src/pages/BookSaved.js
+++ client/src/pages/BookSaved.js
@@ -16,44 +16,52 @@ class BookSaved extends Component {
         event.preventDefault();
         var bookClicked = event.target.id;
         console.log(bookClicked);
-       
+
         API.deleteBook(bookClicked).then(res => this.renderSavedBooks()).catch(err => console.log(err));
         console.log("Clicked delete book!");
     }
-    /*
-    deleteBook = id => {
-        API.deleteBook(id)
-          .then(res => this.loadBooks())
-          .catch(err => console.log(err));
-      };*/
 
     renderSavedBooks = () => {
-        API.getSavedBooks().then(res =>this.setState({ savedBooksData: res.data })).catch(err => console.log(err));
+        API.getSavedBooks().then(res => this.setState({ savedBooksData: res.data })).catch(err => console.log(err));
         console.log(this.state.savedBooksData)
     }
 
     render() {
+        var currentSavedBooks = this.state.savedBooksData;
+        if (currentSavedBooks.length !== 0) {
+            return (
+                <div>
+                    <div className="container">
+                        <row>
+                            <h3><strong>Saved Books</strong></h3>
+                            {this.state.savedBooksData.map(book => (
+                                <BookSavedComponent
+                                    bookID={book._id}
+                                    title={book.title}
+                                    author={book.author}
+                                    link={book.link}
+                                    imageURL={book.imageURL}
+                                    description={book.description}
+                                    deleteBook={this.deleteBook}
+                                />
+                            ))
+                            }
+                        </row>
+                    </div>
+                    }
+            </div>
+            )
+        }
         return (
             <div>
                 <div className="container">
                     <row>
                         <h3><strong>Saved Books</strong></h3>
-                        {this.state.savedBooksData.map(book => (
-                            <BookSavedComponent
-                                bookID={book._id}
-                                title={book.title}
-                                author={book.author}
-                                link={book.link}
-                                imageURL={book.imageURL}
-                                description={book.description}
-                                deleteBook={this.deleteBook}
-                            />
-                        ))
-                        }
+                        <p>No saved books</p>
                     </row>
                 </div>
                 }
-            </div>
+        </div>
         )
     }
 }
